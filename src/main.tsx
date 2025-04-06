75a9768c5872e77833cdbf1c1fbaa14a4705b16b
André Gabriel <andreon098@gmail.com>
2025-04-06 10:28:11 -0300

    feat: setup tanstack router on main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,25 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { RouterProvider, createRouter } from '@tanstack/react-router';
 import './index.css';
 
+// Import the generated route tree
+import { routeTree } from './routeTree.gen';
+
+// Create a new router instance
+const router = createRouter({ routeTree });
+
+// Register the router instance for type safety
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router;
+  }
+}
 const rootElement = document.getElementById('root');
 if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
-      <h1>Hello</h1>
+      <RouterProvider router={router} />
     </StrictMode>,
   );
 }
